1ed3eef4c73e15751c98ad36c135c7b1ce0180c2
Yossra Dridi <yossra.dridi@it-mma.com>
2025-12-14 17:41:59 +0100

    Change ingredient selection source to 'FRUIT_NAME'

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ pd_df = my_dataframe
 # -------------------------------
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients: ", 
-    my_dataframe["SEARCH_ON"].tolist(), 
+    my_dataframe["FRUIT_NAME"].tolist(), 
     max_selections=5
 )
 
